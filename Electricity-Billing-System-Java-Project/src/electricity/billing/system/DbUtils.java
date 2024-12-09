844b4a906faa7fa25019cb3ca3a3d05701e1ec3d
Mspanti <134880830+Mspanti@users.noreply.github.com>
2024-12-09 14:56:49 +0530

    Add files via upload

---
+++ Electricity-Billing-System-Java-Project/src/electricity/billing/system/DbUtils.java
@@ -0,0 +1,14 @@
+package electricity.billing.system;
+
+import java.sql.ResultSet;
+
+import javax.swing.table.TableModel;
+
+public class DbUtils {
+
+    public static TableModel resultSetToTableModel(ResultSet resultSet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resultSetToTableModel'");
+    }
+
+}
